e6ee717a038c912ba413d55adc7ee686e61ec48c
Ayushsingh07 <nqu7069@gmail.com>
2022-11-09 22:20:42 +0530

    v

--- src/index.js
+++ src/index.js
@@ -16,5 +16,6 @@ app.use(cors());
 // routes
 app.use('/tracks', trackRoutes);
 
-app.listen(PORT);
-console.log('Server on port', PORT);
+app.listen(PORT,()=>{
+    console.log("running good gg ON")
+})  
