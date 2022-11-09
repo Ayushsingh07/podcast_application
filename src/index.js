81c3419403ac1f5fc39f8c71fb496bedb77b05b8
Ayushsingh07 <nqu7069@gmail.com>
2022-11-09 19:37:45 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const cors = require('cors');
 const morgan = require('morgan');
+const dotenv=require("dotenv")
+
 
 const trackRoutes = require('./routes/tracks');
 
 const app = express();
 
+const PORT =process.env.PORT || 5000
 // middlewares
 app.use(morgan('dev'));
 app.use(cors());
@@ -14,4 +17,4 @@ app.use(cors());
 app.use('/tracks', trackRoutes);
 
 app.listen(3000);
-console.log('Server on port', 3000);
+console.log('Server on port', PORT);
